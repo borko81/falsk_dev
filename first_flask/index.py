039f8f6d9a5748df088d3c5f96ba015c939eea3e
Borko Laptop <korea60@abv.bg>
2020-01-01 12:48:25 +0200

    Second shance

--- first_flask/index.py
+++ first_flask/index.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import feedparser
 
 app = Flask(__name__)
+URL_RSS = 'https://www.kaldata.com/софтуер/feed'
 
 
 @app.route('/')
 def index():
-    return "RSS Feed News"
+    parser = feedparser.parse(URL_RSS)
+    first_arcticle = parser
+    return f"{first_arcticle}"
 
 
 if __name__ == '__main__':
