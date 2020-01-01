769a1a1a2bc500194d1cd218a09f6c09d50a1c28
Borko Laptop <korea60@abv.bg>
2020-01-01 16:26:03 +0200

    Finish

--- first_flask/index.py
+++ first_flask/index.py
@@ -1,15 +1,13 @@
-from flask import Flask
-import feedparser
+from flask import Flask, render_template
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
 
 app = Flask(__name__)
-URL_RSS = 'https://www.kaldata.com/софтуер/feed'
 
 
 @app.route('/')
-def index():
-    parser = feedparser.parse(URL_RSS)
-    first_arcticle = parser
-    return f"{first_arcticle}"
+@app.route('/<name>')
+def index(name=None):
+    return render_template('base.html', name=name)
 
 
 if __name__ == '__main__':
